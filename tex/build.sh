36f5ff133e3207de4030cf96e2afa8b0a4047cc3
Volodya Larin <volodyalarin@bk.ru>
2021-08-28 19:24:53 +0300

    update

--- tex/build.sh
+++ tex/build.sh
@@ -3,6 +3,6 @@ LATEX_ARGS=-shell-escape
 echo ${LATEX_ARGS}
 rm *.aux *.toc *.out *.log *.bbl *.blg
 pdflatex ${LATEX_ARGS} rpz.tex
-bibtex rpz
+bibtex8 -B rpz
 pdflatex ${LATEX_ARGS} rpz.tex 
 pdflatex ${LATEX_ARGS} rpz.tex 
